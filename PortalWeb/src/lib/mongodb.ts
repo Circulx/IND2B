a1d30be75f12e4f951bc81c9cc599f94cd44ea4f
Circulx <prod.circ@i10ai.com>
2026-02-14 17:09:34 +0530

    f

--- PortalWeb/src/lib/mongodb.ts
+++
@@ -1,5 +0,0 @@
-// Re-export the connectProfileDB function as connectToProfileDB for consistency
-export { connectProfileDB as connectToProfileDB } from "./profileDb"
-
-// Also export other commonly used functions
-export { disconnectProfileDB } from "./profileDb"
